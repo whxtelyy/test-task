52ffa4aaa633515fdffa33e68037306451f1bed9
whxtelyy <isaev_saveliy_d@mail.ru>
2025-04-17 02:13:35 +0500

    Create run.py
    Add run.py

---
+++ run.py
@@ -0,0 +1,35 @@
+import json
+
+
+def check_capacity(max_capacity: int, guests: list) -> bool:
+    datas = []
+    occupied_rooms = 0
+    if 0 < max_capacity < 10**5:
+        for guest in guests:
+            if guest['check-in'] < guest['check-out']:
+                datas.append((guest['check-in'],1))
+                datas.append((guest['check-out'],-1))
+        for data, status in sorted(datas):
+            occupied_rooms += status
+            if occupied_rooms > max_capacity:
+                return False
+    return True
+
+
+if __name__ == "__main__":
+    # Чтение входных данных
+    # Первая строка - вместимость гостиницы
+    max_capacity = int(input())
+    # Вторая строка - количество записей о гостях
+    n = int(input())
+
+    guests = []
+    # Читаем n строк, json-данные о посещении.
+    for _ in range(n):
+        guest = json.loads(input())
+        guests.append(guest)
+
+    # Вызов функции
+    result = check_capacity(max_capacity, guests)
+    # Вывод результата
+    print(result)
