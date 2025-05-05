5e261290aa1bc805d33422caf6a36c735454b619
whxtelyy <163649928+whxtelyy@users.noreply.github.com>
2025-05-05 14:49:50 +0500

    Added keys search with neighboring points check
    so far the tests are not passing

--- run2.py
+++ run2.py
@@ -23,20 +23,37 @@ def min_steps_to_collect_all_keys(data):
                 robots.append((row, column))
             elif cell in keys_char:
                 keys_positions[(row, column)] = cell
-    keys = set(keys_positions.values())
+    set_keys = set(keys_positions.values())
 
-    points = robots[:]
-    for coord in keys_positions:
-        points.append(coord)
 
-    quantity = len(points)
-    distance = [dict() for _ in range(quantity)]
+    def bfs(start, keys):
+        queue = collections.deque([(start[0], start[1], 0)])
+        need_keys = []
+        visited = {start}
 
-    def bfs(point):
-        points_x, points_y = points[point]
-        queue = collections.deque([point])
-        queue.append((points_x, points_y, 0, set()))
-        visited = {(points_x, points_y)}
+        while queue:
+            x_key, y_key, distance = queue.popleft()
+            cell = data[x_key][y_key]
+            if (x_key,y_key) in keys_positions:
+                key = keys_positions[(x_key,y_key)]
+                if key not in keys:
+                    need_keys.append((key, distance, (x_key,y_key)))
+                    continue
+
+            for dist_x, dist_y in ((1,0),(-1,0),(0,1),(0,-1)):
+                next_x, next_y = x + dist_x, y + dist_y
+                if (0 <= next_x < x) and (0 <= next_y < y):
+                    if (next_x, next_y) not in visited:
+                        next_cell = data[next_x][next_y]
+                        if next_cell != "#":
+                            continue
+                        if next_cell in doors_char and next_cell.lower() not in keys:
+                            continue
+                        visited.add((next_x, next_y))
+                        queue.append((next_x, next_y, distance + 1,))
+        return need_keys
+
+    #тут будет подсчет шагов и поиск минимального кол-ва шагов
 
 
 def main():
