d1ccc9d8e1d94225486f4f79164229c7e92cee71
whxtelyy <163649928+whxtelyy@users.noreply.github.com>
2025-05-09 14:09:16 +0500

    Working version (added dfs function)
    finally the tests work :)

--- run2.py
+++ run2.py
@@ -1,4 +1,5 @@
 import sys
+
 import collections
 
 
@@ -15,7 +16,7 @@ def get_input():
 def min_steps_to_collect_all_keys(data):
     robots = []
     keys_positions = {}
-    x,y= len(data), len(data[0])
+    x, y = len(data), len(data[0])
     for row in range(x):
         for column in range(y):
             cell = data[row][column]
@@ -23,6 +24,7 @@ def min_steps_to_collect_all_keys(data):
                 robots.append((row, column))
             elif cell in keys_char:
                 keys_positions[(row, column)] = cell
+
     set_keys = set(keys_positions.values())
 
 
@@ -33,8 +35,7 @@ def min_steps_to_collect_all_keys(data):
 
         while queue:
             x_key, y_key, distance = queue.popleft()
-            cell = data[x_key][y_key]
-            if (x_key,y_key) in keys_positions:
+            if (x_key, y_key) in keys_positions:
                 key = keys_positions[(x_key,y_key)]
                 if key not in keys:
                     need_keys.append((key, distance, (x_key,y_key)))
@@ -42,18 +43,51 @@ def min_steps_to_collect_all_keys(data):
 
             for dist_x, dist_y in ((1,0),(-1,0),(0,1),(0,-1)):
                 next_x, next_y = x_key + dist_x, y_key + dist_y
-                if (0 <= next_x < x) and (0 <= next_y < y):
-                    if (next_x, next_y) not in visited:
-                        next_cell = data[next_x][next_y]
-                        if next_cell == "#":
-                            continue
-                        if next_cell in doors_char and next_cell.lower() not in keys:
-                            continue
-                        visited.add((next_x, next_y))
-                        queue.append((next_x, next_y, distance + 1,))
+                if (0 <= next_x < x) and (0 <= next_y < y) and (next_x, next_y) not in visited:
+                    next_cell = data[next_x][next_y]
+                    if next_cell == "#":
+                        continue
+                    if next_cell in doors_char and next_cell.lower() not in keys:
+                        continue
+                    visited.add((next_x, next_y))
+                    queue.append((next_x, next_y, distance + 1))
+
         return need_keys
 
-    #тут будет подсчет шагов и поиск минимального кол-ва шагов
+    cache = {}
+
+    def dfs(tuple_robots_positions, collected):
+        queue = collections.deque([(tuple_robots_positions, collected, 0)])
+        min_steps = None
+        steps = {}
+
+        while queue:
+            robot_position, collected_keys, distance = queue.popleft()
+            robots_keys = (robot_position, tuple(sorted(collected_keys)))
+            if robots_keys in cache:
+                continue
+            if set(collected_keys) == set_keys:
+                if min_steps is None or distance < min_steps:
+                    min_steps = distance
+                cache[robots_keys] = distance
+                continue
+
+            cache[robots_keys] = None
+            for index, position in enumerate(robot_position):
+                for key, dist, coordinates in bfs(position, set(collected_keys)):
+                    if key in collected_keys:
+                        continue
+                    new_position = list(robot_position)
+                    new_position[index] = coordinates
+                    new_keys = collected_keys + [key]
+                    new_robots_keys = (tuple(new_position), tuple(sorted(new_keys)))
+                    if new_robots_keys not in steps or distance + dist < steps[new_robots_keys]:
+                        steps[new_robots_keys] = distance + dist
+                        queue.append((tuple(new_position), new_keys, distance + dist))
+
+        return min_steps
+
+    return dfs(tuple(robots), [])
 
 
 def main():
